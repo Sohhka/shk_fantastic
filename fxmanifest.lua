8f278cb4bf2c79625a76c88b906b99100bf2e3fc
sσннкα <40715791+Sohhka@users.noreply.github.com>
2020-06-29 20:42:44 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+games { 'gta5' };
+description 'Loading screen pour Fantastic | Fait avec amour par .Sohhka#2196'
+
+files {
+    'index.html',
+    'script.js',
+    'style.css',
+    'Loading.ogg'
+}
+
+loadscreen 'index.html'
